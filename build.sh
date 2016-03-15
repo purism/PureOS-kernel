103c9dff5e3dc802bc50c733bf9b44056235fae6
Adrian Alves <adrian.alves@puri.sm>
2016-03-15 11:30:09 -0300

    fixed pkg name

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ echo "Obtaining sources..."
 apt-src install linux-image-4.3.0-1-amd64
 
 echo "Patching Kernel with BYD drivers..."
-cd linux-image*
+cd linux*
 cp $basedir/data/byd-mouse/drivers/input/mouse/* drivers/input/mouse/.
 echo "CONFIG_MOUSE_PS2_BYD=y" >> debian/config/config
 
 dch -a "BYD mouse driver."
 echo "Building Kernel..."
 
-apt-src import linux-image --here
+apt-src import linux-image-4.3.0-1-amd64 --here
 cd $basedir
-apt-src build linux-image
+apt-src build linux-image-4.3.0-1-amd64
